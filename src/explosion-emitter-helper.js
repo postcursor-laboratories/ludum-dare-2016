ab2af33f3517c30e3d71d0c21a69927078b41d5d
Kenzie Togami <ket1999@gmail.com>
2016-04-17 11:04:06 -0700

    Fix particle lag by only calling makeParticles once

--- src/explosion-emitter-helper.js
+++ src/explosion-emitter-helper.js
@@ -1,5 +1,3 @@
-
-
 export class ExplosionEmitterHelper {
     configure(game) {
         this.gameRef = game;
@@ -7,12 +5,12 @@ export class ExplosionEmitterHelper {
     }
 
     emit(name, x, y, lifetime, numParticles) {
-        if (!this.emitters[name]) {
-            this.emitters[name] = this.gameRef.add.emitter(0, 0, 100)
-            this.emitters[name].gravity = 300;
-        }
         let emitter = this.emitters[name];
-        emitter.makeParticles(name);
+        if (emitter === undefined) {
+            emitter = this.emitters[name] = this.gameRef.add.emitter(0, 0, numParticles * (lifetime / 10));
+            emitter.gravity = 300;
+            emitter.makeParticles(name);
+        }
         emitter.x = x;
         emitter.y = y;
         emitter.start(true, lifetime, null, numParticles);
