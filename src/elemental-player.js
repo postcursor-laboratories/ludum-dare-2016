a494ea64d19a7b2f303d805dd8f43e6526bac00b
Kenzie Togami <ket1999@gmail.com>
2016-04-16 18:13:25 -0700

    Comment out broken code

--- src/elemental-player.js
+++ src/elemental-player.js
@@ -4,7 +4,7 @@ export class ElementalPlayerDescriptor {
 
     constructor(game, elementalName, unitWidth, unitHeight, jumpSpeed, moveSpeed) {
         this.elementalName = elementalName;
-        this.spritesheetName = spritesheetPath;
+        // this.spritesheetName = spritesheetPath;
         this.jumpSpeed = jumpSpeed;
         this.moveSpeed = moveSpeed;
         game.load.spritesheet(elementalName, "img/elementals" + elementalName + ".png", unitWidth, unitHeight);
