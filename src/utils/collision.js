e8ed61e967f5127e278d88c827e21eb052f0d4ca
Kenzie Togami <ket1999@gmail.com>
2016-04-16 17:41:24 -0700

    Add collision utility.

---
+++ src/utils/collision.js
@@ -0,0 +1,31 @@
+import Phaser from "phaser";
+
+/**
+ * This callback type is called `collisionCallback`. It is for {@link collideBox}.
+ *
+ * @callback collisionCallback
+ * @param {Phaser.Sprite} sprite
+ */
+// yes, JSDoc is weird.
+/**
+ * Runs a collision on a group.
+ *
+ * @param x {Number} The X coord, in the center
+ * @param y {Number} The Y coord, in the center
+ * @param width {Number} The width of the box
+ * @param height {Number} The height of the box
+ * @param group {Phaser.Group} The group to check in
+ * @param callback {collisionCallback} Callback for each collided sprite
+ */
+export function collideBox(x, y, width, height, group, callback) {
+    const collideRect = new Phaser.Rectangle(x - width / 2, y - height / 2, width, height);
+    group.forEach(function (sprite) {
+        if (sprite.body) {
+            let /*the*/ body /*hit the floor*/ = sprite.body;
+            let otherRect = new Phaser.Rectangle(body.x, body.y, body.width, body.height);
+            if (collideRect.intersects(otherRect)) {
+                callback(sprite);
+            }
+        }
+    }, this);
+}
