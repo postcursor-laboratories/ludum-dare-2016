32ca2837c4a04c94c7c80bb73ca484ab386f762c
KenyonPrater <kprater.14@gmail.com>
2016-04-17 22:01:47 -0700

    AAAAAAAAAAAAAAAAAAA

--- src/spells/fire-spells.js
+++ src/spells/fire-spells.js
@@ -100,7 +100,7 @@ export class HeatwaveSpell extends Spell
 			flame.sprite.body.velocity.x = 0;
 			flame.sprite.body.velocity.y = -150;
 			flame.update = () => {
-				fireball.checkCollision();
+				flame.checkCollision();
 				if((flame.sprite.body.touching.down || flame.sprite.body.onFloor()))
 				{
 					flame.sprite.body.velocity.y = 0;
