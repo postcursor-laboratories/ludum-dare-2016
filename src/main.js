c66891915508a8fddb8b5b7745863aee440ad4b1
KenyonPrater <kprater.14@gmail.com>
2016-04-17 17:39:41 -0700

    SO IT SEEMS CASE SENSITIVITY IS A THING

--- src/main.js
+++ src/main.js
@@ -31,7 +31,7 @@ class MainGame extends Game {
             GameConfigurable.of(game => {
                 this.elementalPlayers = [
                     new ElementalPlayerDescriptor(game, "human", 32, 32, 400, 300, 5, 4, [4, 2, 4, 1]),
-                    new ElementalPlayerDescriptor(game, "earth", 32, 32, 200, 100, 3, 4, [4, 2, 4, 1], new EarthSpells.RockThrowSpell(), new EarthSpells.fissureSpell()),
+                    new ElementalPlayerDescriptor(game, "earth", 32, 32, 200, 100, 3, 4, [4, 2, 4, 1], new EarthSpells.RockThrowSpell(), new EarthSpells.FissureSpell()),
                     new ElementalPlayerDescriptor(game, "water", 32, 32, 300, 200, 6, 4, [4, 4, 4, 2]),
                     new ElementalPlayerDescriptor(game, "fire" , 32, 32, 350, 300, 6, 4, [4, 4, 4, 4]),
                     new ElementalPlayerDescriptor(game, "air"  , 32, 32, 400, 400, 4, 4, [4, 4, 4, 4])
