5c513918a99dbc8e1d31e998040f7f922b16fc5e
KenyonPrater <kprater.14@gmail.com>
2016-04-17 18:21:08 -0700

    Maybe surf?

--- src/spells/water-spells.js
+++ src/spells/water-spells.js
@@ -7,6 +7,8 @@ import {Spell} from "./spell"
 const SURF_NAME = "Surf";
 const SURF_MANA = 10;
 const SURF_COOLDOWN = 1;
+const SURF_SPEED = 100;
+const SURF_DURATION = 1000;
 
 export class SurfSpell extends Spell {
 	
@@ -22,6 +24,28 @@ export class SurfSpell extends Spell {
 	
 	castSpell(playerObj) {
 		
+		let game = playerObj.gameRef;
+		let facingSign = (playerObj.facing == DIRECTION.LEFT ? -1 : 1);
+		let xCoord = playerObj.sprite.x + (facingSign * 8);
+		let yCoord = playerObj.sprite.y - 48;
+		
+		let surfHandler = new Sprite("waterParticle", xCoord, yCoord); // we need a small, near invisible sprite
+		surfHandler.configure(game);
+		game.promethium.allSprites.push(surfHandler);
+		
+		playerObj.setControlOverride(true); //take that!
+		
+		surfHandler.update = () => {
+			surfHandler.setPosition(playerObj.sprite.x,playerObj.sprite.y);
+			game.promethium.ezEmit.emit("waterParticle", surfHandler.sprite.x, surfHandler.sprite.y, 1000, 5);
+			playerObj.sprite.body.velocity.x= SURF_SPEED * facingSign;
+		}
+		
+		let timer = game.time.create(true);
+		timer.add(SURF_DURATION, () => {
+			playerObj.setControlOverride(false);
+			playerObj.sprite.body.velocity.x= SURF_SPEED *.3* facingSign;
+		}
 	}
 }
 
